a38e38955cd25df4d2d3c22454ab38c29fc15647
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 14:05:19 +0000

    Add files via upload

---
+++ Test Marker.py
@@ -0,0 +1,8 @@
+# Test Marker by Coda
+
+print('Test Marker V1.10 by Coda')
+test_markI = int(input('What is the first persons mark?: '))
+test_markII = int(input('What is the second persons mark?: '))
+test_markIII = int(input('What is the final persons mark?: '))
+answer = (test_markI + test_markII + test_markIII) / 3
+print(answer)
